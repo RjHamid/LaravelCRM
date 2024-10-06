5a2e40c64422b9f16d70670c01a34d0101cccbd2
RjHamid <hamidj1383@gmail.com>
2024-10-06 22:19:18 +0330

    edit migrate carts:price_unit and order:price_total and edit model cart ,order

--- app/Models/Carts.php
+++ app/Models/Carts.php
@@ -4,8 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Carts extends Model
 {
     use HasFactory;
+    protected $fillable =[
+        'user_id',  
+        'product_id',  
+        'unique_code',  
+        'count',  
+        'price_unit',  
+        'status',
+    ];
+    public function order(): HasMany
+    {
+        return $this->hasMany(Order::class);
+    }
+    public function user():BelongsTo  
+    {  
+        return $this->belongsTo(User::class);  
+    }  
+
+    // تعریف رابطه با مدل Product  
+    public function product(): BelongsTo
+    {  
+        return $this->belongsTo(Products::class);  
+    }  
 }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Order extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'unique_code',  
+        'address_id',  
+        'gate',  
+        'price_total',  
+        'transaction_id',  
+        'status', 
+    ];
+    public function carts(): BelongsTo
+    {
+        return $this->belongsTo(Carts::class);
+    }
 }
