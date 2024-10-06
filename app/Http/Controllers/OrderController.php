38aff5c360fe0c2bfe553bda9e3b6c0780e0deeb
RjHamid <hamidj1383@gmail.com>
2024-10-06 21:05:33 +0330

    create order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class OrderController
 {
-    //
+    public function create(Request $request){
+
+        $order=Order::create($request->toArray());
+
+        return response()->json($order);
+    }
 }
