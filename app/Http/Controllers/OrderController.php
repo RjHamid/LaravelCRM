34339dc1f13607abf90eff2018dbf150e41e7479
RjHamid <hamidj1383@gmail.com>
2024-10-07 14:33:54 +0330

    edit api create order edit controller order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -13,4 +13,30 @@ class OrderController
 
         return response()->json($order);
     }
+    public function index($id = null)
+    {
+        if ($id) {
+            $order = Order::where('id', $id)->first();
+        } else {
+            $order = Order::get();
+        }
+
+        return response()->json($order);
+    }
+
+    public function edit(Request $request, $id)
+    {
+        $order = Order::where('id', $id)
+            ->update($request->toArray());
+
+        return response()->json($order);
+    }
+
+    public function delete($id)
+    {
+        Order::where('id', $id)->delete();
+
+        return response()->json('order deleted successfully!');
+    }
 }
+
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\OrderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('Order')->group(function(){
+    Route::post('create',[OrderController::class],'create')->name('create');
+    Route::get('index/{id?}',[OrderController::class],'index')->name('index');
+    Route::put('edit/{id}',[OrderController::class],'edit')->name('edit');
+    Route::delete('delete',[OrderController::class],'delete')->name('delete');
+});
