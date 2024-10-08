000d43c096b1fc14e6d05af1243ebd48de727f4e
RjHamid <hamidj1383@gmail.com>
2024-10-08 12:10:21 +0330

    edit api delete

--- routes/api.php
+++ routes/api.php
@@ -11,23 +11,23 @@ Route::prefix('Order')->group(function(){
     Route::post('create',[OrderController::class],'create')->name('create');
     Route::get('index/{id?}',[OrderController::class],'index')->name('index');
     Route::put('edit/{id}',[OrderController::class],'edit')->name('edit');
-    Route::delete('delete',[OrderController::class],'delete')->name('delete');
+    Route::delete('delete/{id}',[OrderController::class],'delete')->name('delete');
 });
 Route::prefix('Carts')->group(function(){
     Route::post('create',[CartsController::class],'create')->name('create');
     Route::get('index/{id?}',[CartsController::class],'index')->name('index');
     Route::put('edit/{id}',[CartsController::class],'edit')->name('edit');
-    Route::delete('delete',[CartsController::class],'delete')->name('delete');
+    Route::delete('delete/{id}',[CartsController::class],'delete')->name('delete');
 });
 Route::prefix('Payment')->group(function(){
     Route::post('create',[PaymentController::class],'create')->name('create');
     Route::get('index/{id?}',[PaymentController::class],'index')->name('index');
     Route::put('edit/{id}',[PaymentController::class],'edit')->name('edit');
-    Route::delete('delete',[PaymentController::class],'delete')->name('delete');
+    Route::delete('delete/{id}',[PaymentController::class],'delete')->name('delete');
 });
 Route::prefix('Rating')->group(function(){
     Route::post('create',[RatingController::class],'create')->name('create');
     Route::get('index/{id?}',[RatingController::class],'index')->name('index');
     Route::put('edit/{id}',[RatingController::class],'edit')->name('edit');
-    Route::delete('delete',[RatingController::class],'delete')->name('delete');
+    Route::delete('delete/{id}',[RatingController::class],'delete')->name('delete');
 });
