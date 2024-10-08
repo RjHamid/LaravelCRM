3864498a815e346359b2916a13b95a7dc4ace444
RjHamid <hamidj1383@gmail.com>
2024-10-08 12:03:02 +0330

    edit cart: price_unit

--- database/migrations/2024_10_06_073131_create_carts_table.php
+++ database/migrations/2024_10_06_073131_create_carts_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->foreignId('product_id')->constrained();
             $table->string('unique_code')->unique();
             $table->integer('count');
-            $table->string('price_unit');
+            $table->integer('price_unit');
             $table->string('status');
             $table->index('unique_code');
             $table->timestamps();
