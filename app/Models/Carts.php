b97109eb16d27d0efb9814393e2954c05301cb9d
RjHamid <hamidj1383@gmail.com>
2024-10-08 12:03:50 +0330

    edit model:cart

--- app/Models/Carts.php
+++ app/Models/Carts.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Carts extends Model
 {
     use HasFactory;
+
+    protected $fillable = [  
+        'user_id',  
+        'product_id',  
+        'unique_code',  
+        'count',  
+        'price_unit',  
+        'status',  
+    ];  
+
+    // رابطه با مدل User  
+    public function user():BelongsTo 
+    {  
+        return $this->belongsTo(User::class);  
+    }  
+
+    // رابطه با مدل Product  
+    public function product():BelongsTo
+    {  
+        return $this->belongsTo(Products::class);  
+    }
 }
